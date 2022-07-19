8f8f0f8ffa1e37721350f4ce622d03707b769337
Eduardo <eduardo.vazquez.7e5@itb.cat>
2022-07-19 09:54:44 +0200

    Finalitzada Calculadora 2

--- main.js
+++ main.js
@@ -4,9 +4,35 @@ var operator = '';
 var previousNumber = false;
 var elementHighlighted = '';
 
+var numbersEnabled = false;
+var operationsEnabled = false;
+var commaEnabled = false;
+var ceEnabled = false;
+var equalsEnabled = false;
+var cEnabled = false;
+
 /*Connecting all buttons*/
 
 window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("button-1").onclick = function(){Input(1)};
+    document.getElementById("button-2").onclick = function(){Input(2)};
+    document.getElementById("button-3").onclick = function(){Input(3)};
+    document.getElementById("button-4").onclick = function(){Input(4)};
+    document.getElementById("button-5").onclick = function(){Input(5)};
+    document.getElementById("button-6").onclick = function(){Input(6)};
+    document.getElementById("button-7").onclick = function(){Input(7)};
+    document.getElementById("button-8").onclick = function(){Input(8)};
+    document.getElementById("button-9").onclick = function(){Input(9)};
+    document.getElementById("button-0").onclick = function(){Input(0)};
+    document.getElementById("button-divide").onclick = function(){Operator('divide')};
+    document.getElementById("button-multiply").onclick = function(){Operator('multiply')};
+    document.getElementById("button-minus").onclick = function(){Operator('minus')};
+    document.getElementById("button-plus").onclick = function(){Operator('plus')};
+    document.getElementById("button-change").onclick = function(){ButtonChange()};    
+    document.getElementById("button-ce").onclick = function(){ButtonCE()};
+    document.getElementById("button-comma").onclick = function(){InputComma()};
+    document.getElementById("button-equals").onclick = function(){Equals()};
+    document.getElementById("button-c").onclick = function(){ButtonC()};
     EnableAll();
 });
 
@@ -81,86 +107,110 @@ document.addEventListener("keydown", function(event) {
 
 function Input(digit)
 {
-    var display = document.getElementById('display-text');
-    var resultat = display.textContent;
-    if(IsEmptyText(resultat))
-        resultat = '';
-
-    switch(TrueLength(resultat))
+    if(numbersEnabled)
     {
-        case 0:
-            resultat = digit;
-            break;
-        case 1:
-            if(resultat == '0')
-                resultat = digit;
-            else
-                resultat = resultat.concat(digit);
-                if(digit != 0) EnableCE();
-            break;
-        case 9:
-            resultat = resultat.concat(digit);
-            DisableNumbers();
-            DisableButton('comma');
-            break;
-        case 10:
-            break;
-        default:
-            resultat = resultat.concat(digit);
-            break;
-    }
+        var display = document.getElementById('display-text');
+        var resultat = display.textContent;
+        if(IsEmptyText(resultat))
+            resultat = '';
 
-    display.textContent = resultat;
-    previousNumber = false;
+        switch(TrueLength(resultat))
+        {
+            case 0:
+                resultat = digit;
+                break;
+            case 1:
+                if(resultat == '0')
+                    resultat = digit;
+                else
+                    resultat = resultat.concat(digit);
+                    if(digit != 0) {
+                        EnableButton('ce');
+                        ceEnabled = true;
+                    }
+                break;
+            case 9:
+                resultat = resultat.concat(digit);
+                DisableNumbers();
+                DisableButton('comma');
+                commaEnabled = false;
+                break;
+            case 10:
+                break;
+            default:
+                resultat = resultat.concat(digit);
+                break;
+        }
+
+        display.textContent = resultat;
+        previousNumber = false;
+    }
 }
 
 function InputComma()
 {
-    var display = document.getElementById('display-text');
-    if(IsEmptyText(display.textContent))
-        display.textContent = '0,';
-    else if(!display.textContent.includes(',') && TrueLength(display.textContent) <= 9)
-        display.textContent = display.textContent.concat(',');
-    previousNumber = false;
-    DisableButton('comma');
+    if(commaEnabled)
+    {
+        var display = document.getElementById('display-text');
+        if(IsEmptyText(display.textContent))
+            display.textContent = '0,';
+        else if(!display.textContent.includes(',') && TrueLength(display.textContent) <= 9)
+            display.textContent = display.textContent.concat(',');
+        previousNumber = false;
+        DisableButton('comma');
+        commaEnabled = false;
+    }
 }
 
 function ButtonC()
 {
-    var display = document.getElementById('display-text');
-    display.textContent = '0';
-    operator = '';
-    HighLight('');
-    EnableAll();
+    if(cEnabled)
+    {
+        var display = document.getElementById('display-text');
+        display.textContent = '0';
+        operator = '';
+        HighLight('');
+        EnableAll();
+    }
 }
 
 function ButtonCE()
 {
-    var display = document.getElementById('display-text');
-    if(display.textContent.length > 1 && !previousNumber)
+    if(ceEnabled)
     {
-        if(TrueLength(display.textContent) == 10)
-            EnableNumbers();
-        if(display.textContent[display.textContent.length-1] == ',')
-            EnableComma();
-        display.textContent = display.textContent.slice(0,display.textContent.length-1);
-    }
-    else{
-        display.textContent = '0';
-        DisableButton('ce');
+        var display = document.getElementById('display-text');
+        if(display.textContent.length > 1 && !previousNumber)
+        {
+            if(TrueLength(display.textContent) == 10)
+                EnableNumbers();
+            display.textContent = display.textContent.slice(0,display.textContent.length-1);
+            if(!display.textContent.includes(','))
+            {
+                EnableButton('comma');
+                commaEnabled = true;
+            }
+        }
+        else{
+            display.textContent = '0';
+            DisableButton('ce');
+            ceEnabled = false;
+        }
     }
 }
 
 function ButtonChange()
 {
-    var display = document.getElementById('display-text');
-    var valor = ConvertToFloat(display.textContent);
-    if(valor != 0 && !IsEmptyText(display.textContent))
+    if(operationsEnabled)
     {
-        if(display.textContent[0] == '-')
-            display.textContent = display.textContent.slice(1,display.textContent.length);
-        else
-            display.textContent = '-'.concat(display.textContent);
+        var display = document.getElementById('display-text');
+        var valor = ConvertToFloat(display.textContent);
+        if(valor != 0 && !IsEmptyText(display.textContent))
+        {
+            if(display.textContent[0] == '-')
+                display.textContent = display.textContent.slice(1,display.textContent.length);
+            else
+                display.textContent = '-'.concat(display.textContent);
+        }
     }
 }
 
@@ -168,67 +218,72 @@ function ButtonChange()
 
 function Operator(button)
 {
-    if(operator != '' && !previousNumber)
+    if(operationsEnabled)
     {
-        Equals();
-        operator = '';
+        if(operator != '' && !previousNumber)
+        {
+            Equals();
+            operator = '';
+        }
+        if(operator == '')
+        {
+            var display = document.getElementById('display-text');
+            firstNumber = ConvertToFloat(display.textContent);
+            previousNumber = true;
+            EnableAll();
+        }
+        operator = button;
+        console.log('firstNumber ' + firstNumber + ' ' + operator);
+        HighLight(operator);
     }
-    if(operator == '')
-    {
-        var display = document.getElementById('display-text');
-        firstNumber = ConvertToFloat(display.textContent);
-        previousNumber = true;
-        EnableAll();
-    }
-    operator = button;
-    console.log('firstNumber ' + firstNumber + ' ' + operator);
-    HighLight(operator);
 }
 
 function Equals()
 {
-    if(operator != '' && !previousNumber){
-        var display = document.getElementById('display-text');
-        secondNumber = ConvertToFloat(display.textContent);
-        var result;
-        switch(operator)
-        {
-            case 'divide':
-                result = firstNumber / secondNumber;
-                break;
-            case 'multiply':
-                result = firstNumber * secondNumber;
-                break;
-            case 'minus':
-                result = firstNumber - secondNumber;
-                break;
-            case 'plus':
-                result = firstNumber + secondNumber;
-                break;
+    if(equalsEnabled){
+        if(operator != '' && !previousNumber){
+            var display = document.getElementById('display-text');
+            secondNumber = ConvertToFloat(display.textContent);
+            var result;
+            switch(operator)
+            {
+                case 'divide':
+                    result = firstNumber / secondNumber;
+                    break;
+                case 'multiply':
+                    result = firstNumber * secondNumber;
+                    break;
+                case 'minus':
+                    result = firstNumber - secondNumber;
+                    break;
+                case 'plus':
+                    result = firstNumber + secondNumber;
+                    break;
+            }
+            
+            EnableAll();
+            HighLight('');
+            display.textContent = ResultDisplay(result);
+            console.log('secondNumber ' + secondNumber + ' = result ' + result)
+            operator = '';
+            firstNumber = '';
+            previousNumber = true;
         }
-        
-        EnableAll();
-        HighLight('');
-        display.textContent = ResultDisplay(result);
-        console.log('secondNumber ' + secondNumber + ' = result ' + result)
-        operator = '';
-        firstNumber = '';
-        previousNumber = true;
-    }
-    else if(operator != '' && previousNumber)
-    {
-        var display = document.getElementById('display-text');
-        display.textContent = 'ERROR';
-        console.log('= result ERROR');
-        HighLight('');
-        DisableAll();
-    }
-    else{
-        var display = document.getElementById('display-text');
-        if(display.textContent[display.textContent.length-1]==',')
-            display.textContent = display.textContent.substring(0, display.textContent.length-1);
-        previousNumber = true;
-        EnableAll();
+        else if(operator != '' && previousNumber)
+        {
+            var display = document.getElementById('display-text');
+            display.textContent = 'ERROR';
+            console.log('= result ERROR');
+            HighLight('');
+            DisableAll();
+        }
+        else{
+            var display = document.getElementById('display-text');
+            if(display.textContent[display.textContent.length-1]==',')
+                display.textContent = display.textContent.substring(0, display.textContent.length-1);
+            previousNumber = true;
+            EnableAll();
+        }
     }
 }
 
@@ -237,60 +292,50 @@ function Equals()
 function EnableAll()
 {
     EnableNumbers();
-    EnableComma();
     EnableOperations();
-    EnableC();
+    EnableButton('comma');
+    EnableButton('c');
+    EnableButton('equals');
     DisableButton('ce');
-    EnableEquals();
+    commaEnabled = true;
+    cEnabled = true;
+    equalsEnabled = true;
+    ceEnabled = false;
+
 }
 
 function DisableAll()
 {
     DisableNumbers();
-    DisableButton('comma');
     DisableOperations();
+    DisableButton('comma');
     DisableButton('ce');
     DisableButton('equals');
+    commaEnabled = false;
+    ceEnabled = false;
+    equalsEnabled = false;
 }
 
 function EnableNumbers()
 {
-    for(var i = 0; i<10; i++){
-        var button = document.getElementById('button-' + i.toString());
-        button.classList.remove('not-working-button');
-        button.classList.add('working-button');
-    }
-    document.getElementById("button-1").onclick = function(){Input(1)};
-    document.getElementById("button-2").onclick = function(){Input(2)};
-    document.getElementById("button-3").onclick = function(){Input(3)};
-    document.getElementById("button-4").onclick = function(){Input(4)};
-    document.getElementById("button-5").onclick = function(){Input(5)};
-    document.getElementById("button-6").onclick = function(){Input(6)};
-    document.getElementById("button-7").onclick = function(){Input(7)};
-    document.getElementById("button-8").onclick = function(){Input(8)};
-    document.getElementById("button-9").onclick = function(){Input(9)};
-    document.getElementById("button-0").onclick = function(){Input(0)};
+    for(var i = 0; i<10; i++)
+        EnableButton(i.toString());
+    numbersEnabled = true;
 }
 
 function DisableNumbers()
 {
     for(var i =0; i<10; i++)
         DisableButton(i.toString());
+    numbersEnabled = false;
 }
 
 function EnableOperations()
 {
     let operation = ["divide", "multiply", "minus", "plus", "change"];
-    for(var i = 0; i<operation.length; i++){
-        var button = document.getElementById('button-' + operation[i]);
-        button.classList.remove('not-working-button');
-        button.classList.add('working-button');
-    }
-    document.getElementById("button-divide").onclick = function(){Operator('divide')};
-    document.getElementById("button-multiply").onclick = function(){Operator('multiply')};
-    document.getElementById("button-minus").onclick = function(){Operator('minus')};
-    document.getElementById("button-plus").onclick = function(){Operator('plus')};
-    document.getElementById("button-change").onclick = function(){ButtonChange()};
+    for(var i = 0; i<operation.length; i++)
+        EnableButton(operation[i]);
+    operationsEnabled = true;
 }
 
 function DisableOperations()
@@ -298,38 +343,14 @@ function DisableOperations()
     let operation = ["divide", "multiply", "minus", "plus", "change"];
     for(var i =0; i<operation.length; i++)
         DisableButton(operation[i]);
+    operationsEnabled = false;
 }
 
-function EnableComma()
+function EnableButton(text)
 {
-    var button = document.getElementById('button-comma');
+    var button = document.getElementById('button-' + text);
     button.classList.remove('not-working-button');
     button.classList.add('working-button');
-    button.onclick = function(){InputComma()};
-}
-
-function EnableCE()
-{
-    var button = document.getElementById('button-ce');
-    button.classList.remove('not-working-button');
-    button.classList.add('working-button');
-    button.onclick = function(){ButtonCE()};
-}
-
-function EnableEquals()
-{
-    var button = document.getElementById('button-equals');
-    button.classList.remove('not-working-button');
-    button.classList.add('working-button');
-    button.onclick = function(){Equals()};
-}
-
-function EnableC()
-{
-    var button = document.getElementById('button-c');
-    button.classList.remove('not-working-button');
-    button.classList.add('working-button');
-    button.onclick = function(){ButtonC()};
 }
 
 function DisableButton(text)
@@ -337,7 +358,6 @@ function DisableButton(text)
     var button = document.getElementById('button-' + text);
     button.classList.remove('working-button');
     button.classList.add('not-working-button');
-    button.onclick = null;
 }
 
 /*Auxiliary Functions*/
