593365c77c794878ed337b70a6116b7cbb1a1a0a
Eduardo <eduardo.vazquez.7e5@itb.cat>
2022-07-21 15:06:29 +0200

    Reworking of Button Availability

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 var firstNumber;
 var secondNumber;
 var operator = '';
-var previousNumber = false;
+var displayIsShowingPreviousNumber = false;
 var elementHighlighted = '';
 
-var numbersEnabled = false;
-var operationsEnabled = false;
-var commaEnabled = false;
-var ceEnabled = false;
-var equalsEnabled = false;
-var cEnabled = false;
-var changeEnabled = false;
+var isButtonAvailable = new Map();
 
 /*Connecting all buttons*/
 
@@ -34,51 +28,22 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById("button-comma").onclick = function(){InputComma()};
     document.getElementById("button-equals").onclick = function(){Equals()};
     document.getElementById("button-c").onclick = function(){ButtonC()};
-    EnableAll();
-    DisableCES();
-    DisableButton('0');
+    SetOperationButtonsAvailability(true);
+    SetPositiveNumbersButtonsAvailability(true);
+    SetButtonsAvailability(false, '0', 'ce', 'change');
 });
 
 /*Connecting all keys*/
 
 document.addEventListener("keydown", function(event) {
     event.preventDefault();
+    if('0123456789'.includes(event.key)){
+        Input(parseFloat(event.key));
+    }
     switch (event.key) {
-        case '0':
-            Input(0);
-            break;
-        case '1':
-            Input(1);
-            break;
-        case '2':
-            Input(2);
-            break;
-        case '3':
-            Input(3);
-            break;
-        case '4':
-            Input(4);
-            break;
-        case '5':
-            Input(5);
-            break;
-        case '6':
-            Input(6);
-            break;
-        case '7':
-            Input(7);
-            break;
-        case '8':
-            Input(8);
-            break;
-        case '9':
-            Input(9);
-            break;
-        case '0':
-            Input(0);
-            break;
         case '+':
             Operator('plus');
+            console.log(isButtonAvailable.get('plus'));
             break;
         case '-':
             Operator('minus');
@@ -111,39 +76,29 @@ document.addEventListener("keydown", function(event) {
 
 function Input(digit)
 {
-    if(numbersEnabled)
+    if(isButtonAvailable.get(digit.toString()))
     {
-        var display = document.getElementById('display-text');
-        var resultat = display.textContent;
-        if(IsEmptyText(resultat))
+        var resultat = GetDisplayText();
+        if(resultat == 'ERROR' || displayIsShowingPreviousNumber)
             resultat = '0';
 
-        switch(TrueLength(resultat))
+        switch(GetNumberOfDigits(resultat))
         {
-            case 0:
-                resultat = digit;
-                break;
             case 1:
                 if(resultat == '0')
                     resultat = digit;
                 else
-                    resultat = resultat.concat(digit);                
+                    resultat = resultat.concat(digit); 
+
                 if(resultat != '0')
-                {
-                    EnableCES();
-                    EnableButton('0');
-                }
+                    SetButtonsAvailability(true, 'ce', 'change', '0');
                 else
-                {
-                    DisableCES();
-                    DisableButton('0');
-                }
+                    SetButtonsAvailability(false, 'ce', 'change', '0');
                 break;
             case 9:
                 resultat = resultat.concat(digit);
-                DisableNumbers();
-                DisableButton('comma');
-                commaEnabled = false;
+                SetPositiveNumbersButtonsAvailability(false);
+                SetButtonsAvailability(false, 'comma', '0');
                 break;
             case 10:
                 break;
@@ -152,88 +107,84 @@ function Input(digit)
                 break;
         }
 
-        display.textContent = resultat;
-        previousNumber = false;
+        SetDisplayText(resultat);
+        displayIsShowingPreviousNumber = false;
     }
 }
 
 function InputComma()
 {
-    if(commaEnabled)
+    if(isButtonAvailable.get('comma'))
     {
-        var display = document.getElementById('display-text');
-        if(IsEmptyText(display.textContent))
-            display.textContent = '0,';
-        else if(!display.textContent.includes(',') && TrueLength(display.textContent) <= 9)
-            display.textContent = display.textContent.concat(',');
-        previousNumber = false;
-        EnableButton('ce');
-        ceEnabled = true;
-        EnableButton('0');
-        DisableButton('comma');
-        commaEnabled = false;
+        var display = GetDisplayText();
+        if(IsEmptyText(display))
+            SetDisplayText('0,');
+        else if(!display.includes(',') && GetNumberOfDigits(display) <= 9)
+            SetDisplayText(display.concat(','));
+        displayIsShowingPreviousNumber = false;
+        SetButtonsAvailability(true, 'ce', '0');
+        SetButtonsAvailability(false, 'comma');
     }
 }
 
 function ButtonC()
 {
-    if(cEnabled)
+    if(isButtonAvailable.get('c'))
     {
-        var display = document.getElementById('display-text');
-        display.textContent = '0';
+        SetDisplayText('0');
         operator = '';
         HighLight('');
-        EnableAll();
-        DisableCES();
-        DisableButton('0');
+        SetOperationButtonsAvailability(true);
+        SetPositiveNumbersButtonsAvailability(true);
+        SetButtonsAvailability(false, '0', 'ce', 'change');
     }
 }
 
 function ButtonCE()
 {
-    if(ceEnabled)
+    if(isButtonAvailable.get('ce'))
     {
-        var display = document.getElementById('display-text');
-        if(TrueLength(display.textContent) > 1 && !previousNumber)
+        var display = GetDisplayText();
+        if(GetNumberOfDigits(display) > 1 && !displayIsShowingPreviousNumber)
         {
-            if(TrueLength(display.textContent) == 10)
-                EnableNumbers();
-            display.textContent = display.textContent.slice(0,display.textContent.length-1);
-            if(!display.textContent.includes(','))
-            {
-                EnableButton('comma');
-                commaEnabled = true;
+            if(GetNumberOfDigits(display) == 10){
+                SetPositiveNumbersButtonsAvailability(true);
+                SetButtonsAvailability(true, '0');
             }
+            display = display.slice(0,display.length-1);
+            if(!display.includes(','))
+                SetButtonsAvailability(true, 'comma');
         }
         else
-            display.textContent = '0';
-        
-        if(display.textContent == '0'){
-            EnableAll();
-            DisableCES();
-            DisableButton('0');
-        }
-        else if(display.textContent == '0,')
+            display ='0';
+
+        SetDisplayText(display);
+
+        if(display == '0')
         {
-            EnableAll();
-            DisableButton('change');
-            changeEnabled = false;
+            SetButtonsAvailability(true, 'comma');
+            SetButtonsAvailability(false, '0', 'ce', 'change');
+        }
+        else if(display == '0,')
+        {
+            SetButtonsAvailability(true, '0', 'ce')
+            SetButtonsAvailability(false, 'change');
         }
     }
 }
 
 function ButtonChange()
 {
-    if(operationsEnabled)
+    if(isButtonAvailable.get('change'))
     {
-        var display = document.getElementById('display-text');
-        var valor = ConvertToFloat(display.textContent);
-        if(valor != 0 && !IsEmptyText(display.textContent))
+        var display = GetDisplayText();
+        var valor = ConvertToFloat(display);
+        if(valor != 0 && !IsEmptyText(display))
         {
-            if(display.textContent[0] == '-')
-                display.textContent = display.textContent.slice(1,display.textContent.length);
+            if(display[0] == '-')
+                SetDisplayText(display.slice(1,display.length));
             else
-                display.textContent = '-'.concat(display.textContent);
+                SetDisplayText('-'.concat(display));
         }
     }
 }
@@ -242,23 +193,25 @@ function ButtonChange()
 
 function Operator(button)
 {
-    if(operationsEnabled)
+    if(isButtonAvailable.get(button))
     {
-        if(operator != '' && !previousNumber)
+        if(operator != '' && !displayIsShowingPreviousNumber)
         {
             Equals();
             operator = '';
         }
     }
-    if(operationsEnabled)
+    if(isButtonAvailable.get(button))
     {
         if(operator == '')
         {
-            var display = document.getElementById('display-text');
-            firstNumber = ConvertToFloat(display.textContent);
-            previousNumber = true;
-            EnableAll();
-            DisableCES();
+            var display = GetDisplayText();
+            firstNumber = ConvertToFloat(display);
+            displayIsShowingPreviousNumber = true;
+            SetPositiveNumbersButtonsAvailability(true);
+            SetOperationButtonsAvailability(true);
+            SetButtonsAvailability(true, '0');
+            SetButtonsAvailability(false, 'ce', 'change');
         }
         operator = button;
         console.log('firstNumber ' + firstNumber + ' ' + operator);
@@ -268,154 +221,88 @@ function Operator(button)
 
 function Equals()
 {
-    if(equalsEnabled){
-        if(operator != '' && !previousNumber){
-            var display = document.getElementById('display-text');
-            secondNumber = ConvertToFloat(display.textContent);
-            var result = Calculate();
+    if(isButtonAvailable.get('equals')){
+        if(operator != '' && !displayIsShowingPreviousNumber){
+            var display = GetDisplayText();
+            secondNumber = ConvertToFloat(display);
+            var result = Calculate(firstNumber, secondNumber, operator);
         
-            EnableAll();
-            DisableCES();
+            SetPositiveNumbersButtonsAvailability(true);
+            SetOperationButtonsAvailability(true);
+            SetButtonsAvailability(true, '0');
+            SetButtonsAvailability(false, 'ce', 'change');
+
             HighLight('');
             var screenText = ResultDisplay(result);
             if(screenText != 'ERROR'){
-                display.textContent = screenText;
+                SetDisplayText(screenText);
                 console.log('secondNumber ' + secondNumber + ' = result ' + result);
             }
             operator = '';
             firstNumber = '';
-            previousNumber = true;
+            displayIsShowingPreviousNumber = true;
         }
-        else if(operator != '' && previousNumber)
+        else if(operator != '' && displayIsShowingPreviousNumber)
         {
             ThrowError();
         }
         else{
-            var display = document.getElementById('display-text');
-            if(display.textContent[display.textContent.length-1]==',')
-                display.textContent = display.textContent.substring(0, display.textContent.length-1);
-            previousNumber = true;
-            EnableAll();
-            DisableCES();
+            var display = GetDisplayText();
+            if(display[display.length-1]==',')
+                SetDisplayText(display.substring(0, display.length-1));
+            displayIsShowingPreviousNumber = true;
+            SetPositiveNumbersButtonsAvailability(true);
+            SetOperationButtonsAvailability(true);
+            SetButtonsAvailability(true, '0');
+            SetButtonsAvailability(false, 'ce', 'change');
         }
-    }
-}
-
-function Calculate()
-{
-    switch(operator)
-    {
-        case 'divide':
-            return firstNumber / secondNumber;
-        case 'multiply':
-            return firstNumber * secondNumber;
-        case 'minus':
-            return firstNumber - secondNumber;
-        case 'plus':
-            return firstNumber + secondNumber;
     }
 }
 
 function ThrowError()
 {
-    var display = document.getElementById('display-text');
-    display.textContent = 'ERROR';
+    SetDisplayText('ERROR');
     console.log('= result ERROR');
     HighLight('');
-    DisableAll();
+    SetPositiveNumbersButtonsAvailability(false);
+    SetOperationButtonsAvailability(false);
+    SetButtonsAvailability(false, 'ce', 'change', '0');
 }
 
-/*Disabling buttons*/
+/*Buttons Availability Methods*/
 
-function EnableAll()
+function SetOperationButtonsAvailability(isAvailable)
 {
-    EnableNumbers();
-    EnableOperations();
-    EnableButton('comma');
-    EnableButton('c');
-    EnableButton('equals');
-    commaEnabled = true;
-    cEnabled = true;
-    equalsEnabled = true;
+    SetButtonsAvailability(isAvailable, 'divide', 'multiply', 'minus', 'plus', 'equals', 'comma', 'c');
 }
 
-function DisableAll()
+function SetPositiveNumbersButtonsAvailability(isAvailable)
 {
-    DisableNumbers();
-    DisableOperations();
-    DisableCES();
-    DisableButton('comma');
-    DisableButton('equals');
-    commaEnabled = false;
-    equalsEnabled = false;
+    SetButtonsAvailability(isAvailable, '1', '2', '3', '4', '5', '6', '7', '8', '9');
 }
 
-function EnableNumbers()
+function SetButtonsAvailability(isAvailable, ...params) 
 {
-    for(var i = 0; i<10; i++)
-        EnableButton(i.toString());
-    numbersEnabled = true;
-}
-
-function DisableNumbers()
-{
-    for(var i =0; i<10; i++)
-        DisableButton(i.toString());
-    numbersEnabled = false;
-}
-
-function EnableOperations()
-{
-    let operation = ["divide", "multiply", "minus", "plus"];
-    for(var i = 0; i<operation.length; i++)
-        EnableButton(operation[i]);
-    operationsEnabled = true;
-}
-
-function DisableOperations()
-{
-    let operation = ["divide", "multiply", "minus", "plus"];
-    for(var i =0; i<operation.length; i++)
-        DisableButton(operation[i]);
-    operationsEnabled = false;
-}
-
-function EnableCES()// CE and +/-  button have a fundamentally similar behaviour
-{
-    let operation = ["ce", "change"];
-    for(var i =0; i<operation.length; i++)
-        EnableButton(operation[i]);
-    ceEnabled = true;
-    changeEnabled = true;
-}
-
-function DisableCES() 
-{
-    let operation = ["ce", "change"];
-    for(var i =0; i<operation.length; i++)
-        DisableButton(operation[i]);
-    ceEnabled = false;
-    changeEnabled = false;
-}
-
-function EnableButton(text)
-{
-    var button = document.getElementById('button-' + text);
-    button.classList.remove('not-working-button');
-    button.classList.add('working-button');
-}
-
-function DisableButton(text)
-{
-    var button = document.getElementById('button-' + text);
-    button.classList.remove('working-button');
-    button.classList.add('not-working-button');
+    for(let i =0; i<params.length; i++){
+        var button = document.getElementById('button-' + params[i]);
+        console.log(params[i]);
+        if(isAvailable){
+            button.classList.remove('not-working-button');
+            button.classList.add('working-button');
+        }
+        else
+        {
+            button.classList.remove('working-button');
+            button.classList.add('not-working-button');
+        }
+        isButtonAvailable.set(params[i], isAvailable);
+    }
 }
 
 /*Auxiliary Functions*/
 
 // Discounts sign and decimal comma
-function TrueLength(text)
+function GetNumberOfDigits(text)
 {
     return text.length - (text.includes(',') ? 1 : 0 ) - (text[0] == '-' ? 1 : 0);
 }
@@ -459,7 +346,7 @@ function HasUnusedDecimalDigits(text)
 
 function IsEmptyText(text)
 {
-    if(text == '' || text == 'ERROR' || previousNumber) return true;
+    if(text == '' || text == 'ERROR' || displayIsShowingPreviousNumber) return true;
     return false;
 }
 
@@ -478,4 +365,29 @@ function HighLight(text)
         var highlight = document.getElementById(elementHighlighted);
         highlight.style.backgroundColor = colorHighLight;
     }
+}
+
+function Calculate(num1, num2, operation)
+{
+    switch(operation)
+    {
+        case 'divide':
+            return num1 / num2;
+        case 'multiply':
+            return num1 * num2;
+        case 'minus':
+            return num1 - num2;
+        case 'plus':
+            return num1 + num2;
+    }
+}
+
+function GetDisplayText()
+{
+    return document.getElementById('display-text').textContent;
+}
+
+function SetDisplayText(text)
+{
+    document.getElementById('display-text').textContent = text;
 }
